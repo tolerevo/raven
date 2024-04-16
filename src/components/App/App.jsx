c2898d4076865417b008476a1174c2bc4ebd9da0
ditole <tolerevo@hotmail.com>
2024-04-16 09:02:16 -0600

    add chakra modules

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./App.module.css";
-//import { Box } from "@chakra-ui/react";//
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className={styles.App}>
       <h1>Ravenous</h1>
       <SearchBar className={styles.SearchBar} />
-      <BusinessList />
+      <Box bgColor="orange.100">
+        <BusinessList />
+      </Box>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-/*TODOimport { ChakraProvider } from "@chakra-ui/react";*/
+import { ChakraProvider } from "@chakra-ui/react";
+import  theme  from "./theme";
 // TODO ver si voy a usar chakra para el estilo o no import { ChakraProvider } from "@chakra-ui/react";//
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <ChakraProvider>*/}
+    <ChakraProvider theme={theme}>
     <App />
-    {/*</ChakraProvider>*/}
+    </ChakraProvider>
   </React.StrictMode>
 );
