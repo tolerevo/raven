b1003d96ba68556e44d3f9a1e23877b9a292d1f3
ditole <tolerevo@hotmail.com>
2024-04-18 09:27:48 -0600

    mend

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./components/App/App";
 /* import { ChakraProvider } from "@chakra-ui/react";
 import  theme  from "./theme";*/ //TODO ver si voy a usar chakra para el estilo o no import { ChakraProvider } from "@chakra-ui/react";//
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
